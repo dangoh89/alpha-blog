8d29606a1250a3a6d026e46e9fdf4b2df6eccff6
dangoh89 <dangoh.sg89@gmail.com>
2017-10-18 06:54:15 +0000

    creatre users table and model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = "Article was sccessfully created"
+            flash[:success] = "Article was successfully created"
             redirect_to article_path(@article)
         else
             render 'new'
