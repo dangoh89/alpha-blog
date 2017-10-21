d526e54757a0892cb6a758a277660cc618d6a785
dangoh89 <dangoh.sg89@gmail.com>
2017-10-21 06:31:59 +0000

    Create show action and add gravity image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,29 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            flash[:success] = "welcome to to alpha blog" #{@user.username}
+            flash[:success] = "welcome to to alpha blog #{@user.username}"
             redirect_to articles_path
         else
             render 'new'
         end
+    end
+    
+    def edit
+        @user = User.find(params[:id])
+    end
+    
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:success] = "Your account was updated successfully"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
+    
+    def show
+        @user = User.find(params[:id])
     end
     
     private
